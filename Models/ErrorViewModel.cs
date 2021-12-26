da3fe422f4702112c492490b1cc0535cec768bff
shailendr.soni <shailendr.soni@hotmail.com>
2021-12-26 20:46:37 +0530

    Application Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jaiganeshmvcapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
